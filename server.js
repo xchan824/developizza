4827bec2bd84dc4ef19e5bfb1425995705adbe43
Christian Paulo Gomonit <xchan824@gmail.com>
2022-07-21 19:57:42 +0800

    added cart page

--- server.js
+++ server.js
@@ -9,14 +9,18 @@ const PORT = process.env.PORT || 3000;
 // assets
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
 // set template engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart');
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
