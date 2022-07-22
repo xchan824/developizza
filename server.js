0f44d2519aa2769ce2f8ad64bfd9da8d9a7d35a5
Christian Paulo Gomonit <xchan824@gmail.com>
2022-07-22 15:07:05 +0800

    connected homepage to mongodb

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const ejs = require('ejs');
 const path = require('path');
 const expressLayout = require('express-ejs-layouts');
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+
+// database connection
+const url = 'mongodb+srv://user1:ECxUcWehwrkeOEBl@developizza.crveg2h.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(url, err => {
+    if(err) throw err;
+    console.log('Connected to database');
+})
 
 // assets
 app.use(express.static('public'));
