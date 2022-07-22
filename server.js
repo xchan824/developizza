86c9e4bf049adb9374f31225cb5e0fc4616e7584
Christian Paulo Gomonit <xchan824@gmail.com>
2022-07-22 19:30:53 +0800

    added register crud and login

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const flash = require('express-flash');
 const MongoDbStore = require('connect-mongo')(session);
+const passport = require('passport');
 
 // database connection
 mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify : true });
@@ -34,15 +35,23 @@ app.use(session({
     cookie: { maxAge: 1000 * 60 * 60 * 24 } // 24 hours
 }));
 
+// passport config
+const passportInit = require('./app/config/passport');
+passportInit(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 // assets
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session;
+    res.locals.user = req.user;
     next();
 });
 
